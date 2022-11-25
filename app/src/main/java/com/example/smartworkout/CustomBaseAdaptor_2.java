22089365ec4c10d22b6e9a71e30d2241cf415bf6
Amir Hossein Mirahmadi <mirahmady1375@gmail.com>
2022-11-24 20:55:32 -0500

    added exercises to sessions

--- app/src/main/java/com/example/smartworkout/CustomBaseAdaptor_2.java
+++ app/src/main/java/com/example/smartworkout/CustomBaseAdaptor_2.java
@@ -2,6 +2,7 @@ package com.example.smartworkout;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +10,12 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class CustomBaseAdaptor_2 extends BaseAdapter {
 
-
+    private ArrayList<Exercise> Exercises;
     Context context;
     String Exercise_Names[];
     int ax[];
@@ -52,15 +55,44 @@ public class CustomBaseAdaptor_2 extends BaseAdapter {
         TextView textView1 = convertView.findViewById(R.id.ExerNameTXT);
         ImageView imageView1 = convertView.findViewById(R.id.ExerImage);
 
-
         textView1.setText(Exercise_Names[position]);
         imageView1.setImageResource(ax[position]);
-
 
         return convertView;
 
 
     }
+    public ArrayList<Exercise> populateExercises(){
+        ArrayList<Exercise> exercises=new ArrayList<Exercise>();
 
+        //chest
+        Exercise e1=new Exercise(1,R.drawable.benchpress,"Bench press","The bench press is a compound exercise that targets the muscles of the upper body. It involves lying on a bench and pressing weight upward using either a barbell or a pair of dumbbells. During a bench press, you lower the weight down to chest level and then press upwards while extending your arms");
+        Exercise e2=new Exercise(2,R.drawable.incline_benchpress,"Incline Bench press","The purpose of the incline press is to focus more of the work on the upper pecs. The main benefit in performing incline presses is to develop the upper portion of the pectoral muscles. When the bench is set at an incline (15 to 30 degrees), you activate your shoulders more since it's comparable to a shoulder press.");
+        Exercise e3=new Exercise(3,R.drawable.decline_benchpress,"Decline Bench press","In a decline bench press, the bench is set to 15 to 30 degrees on a decline. This angle places your upper body on a downward slope, which activates the lower pectoral muscles as you push weights away from your body. When part of a complete chest routine, decline bench presses can help your pecs look more defined.");
+        Exercise e4=new Exercise(4,R.drawable.chest_fly,"Chest Fly","Bring your arms up straight in front of you so they're at chest level, palms facing each other. Extend arms out to the sides, until your arms are extended. Keep arms at chest level the entire time. Bring them back to center. Repeat 10–15 times");
+        exercises.add(e1);
+        exercises.add(e2);
+        exercises.add(e3);
+        exercises.add(e4);
+        //arm
+        Exercise e5=new Exercise(5,R.drawable.bicep_curl,"Bicep Curl","an exercise in which you bend your arm towards your body at the elbow in order to make your biceps (= the large muscle at the front of the upper arm) stronger: Do three sets of biceps curls using 5- to 8-pound weights.");
+        Exercise e6=new Exercise(6,R.drawable.preacher_curl,"Preacher Curl"," a weightlifting exercise for the biceps in which a barbell is lifted by flexing the elbows, with the upper arms resting on an angled bench");
+        Exercise e7=new Exercise(7,R.drawable.triceps_extension,"Triceps Extension","Stand with feet hip width apart with a slight bend in the knees. Grip the bar with palms facing downwards, about shoulder width apart. Keeping your elbows close to your body, take a slight forward lean, and exhale as you push the bar down till your arms full extended.");
+        Exercise e8=new Exercise(8,R.drawable.dip_machine,"Dip Machine","The dip machine is a machine exercise that mimics a triceps dip, a bodyweight exercise performed on parallel bars or on a pull-up and dip station. It targets the triceps first, but also stretches and strengthens the chest and shoulders.");
+        exercises.add(e5);
+        exercises.add(e6);
+        exercises.add(e7);
+        exercises.add(e8);
+        //leg
+        Exercise e9=new Exercise(9,R.drawable.leg_extension,"Leg Extension","Leg extensions are done on a leg extension machine. You sit on the machine with a weighted pad on top of your lower legs. Then you use your quads to repeatedly extend your knees and lift your lower legs. While the leg extension is a great quad workout, it might not be the most practical move");
+        Exercise e10=new Exercise(10,R.drawable.leg_press,"Leg Press","Leg presses are done in a seated position. Your legs repeatedly press against weights, which can be adjusted according to your fitness level. This targets your quads, glutes, hamstrings, hips, and calves. The seated position of leg presses helps keep your upper body and torso still");
+        Exercise e11=new Exercise(11,R.drawable.dead_lift,"Dead lift","The deadlift is a movement in which your hips hinge backward to lower down and pick up a weighted barbell or kettlebell from the floor. Your back is flat throughout the movement. Some benefits of performing deadlifts include strengthening and gaining more definition in your upper and lower back, glutes, and hamstrings");
+        Exercise e12=new Exercise(12,R.drawable.leg_curl,"Leg Curl","The hamstring curl, also called a leg curl, is an exercise that strengthens the hamstrings. It involves bending your knees and moving your heels toward your butt while the rest of your body stays still. Typically, the exercise is done on a leg curl machine");
+        exercises.add(e9);
+        exercises.add(e10);
+        exercises.add(e11);
+        exercises.add(e12);
+        return  exercises;
+    }
 
 }
--- app/src/main/java/com/example/smartworkout/SessionFragment.java
+++ app/src/main/java/com/example/smartworkout/SessionFragment.java
@@ -19,6 +19,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class SessionFragment extends Fragment {
@@ -103,27 +104,40 @@ public class SessionFragment extends Fragment {
 //        if (offset != 0){
 //            chronometer.setBase(SystemClock.elapsedRealtime() - offset);
 //        }
-
+        List<Exercise> e;
+        e=populateExercises();
         switch (Session_Selector){
             case 1:
                 listItem = getResources().getStringArray(R.array.sessionArms);
-                ax = new int[]{R.drawable.ic_baseline_fitness_center_24, R.drawable.ic_baseline_info_24,
-                        R.drawable.ic_baseline_bar_chart_24, R.drawable.ic_baseline_timer_24,R.drawable.ic_baseline_pin_drop_24};
-                title.setText("Arms");
+//                e.get(8).Cover;
+//                e.get(9).Cover;
+//                e.get(10).Cover;
+//                e.get(11).Cover;
+                ax = new int[]{e.get(8).Cover, e.get(9).Cover,
+                        e.get(10).Cover, e.get(11).Cover};
+                title.setText("Legs");
                 this.selector_For_AlertDialog = 1;
                 break;
             case 2:
+//                e.get(4).Cover;
+//                e.get(5).Cover;
+//                e.get(6).Cover;
+//                e.get(7).Cover;
                 listItem = getResources().getStringArray(R.array.sessionLegs);
-                title.setText("Legs");
-                ax = new int[]{R.drawable.ic_baseline_fitness_center_24, R.drawable.ic_baseline_info_24,
-                        R.drawable.ic_baseline_bar_chart_24, R.drawable.ic_baseline_timer_24,R.drawable.ic_baseline_pin_drop_24};
+                title.setText("Arms");
+                ax = new int[]{ e.get(4).Cover,  e.get(5).Cover,
+                        e.get(6).Cover, e.get(7).Cover};
                 this.selector_For_AlertDialog = 2;
                 break;
             case 3:
+//                e.get(0).Cover;
+//                e.get(1).Cover;
+//                e.get(2).Cover;
+//                e.get(3).Cover;
                 listItem = getResources().getStringArray(R.array.sessionChest);
                 title.setText("Chest");
-                ax = new int[]{R.drawable.ic_baseline_bar_chart_24, R.drawable.ic_baseline_info_24,
-                        R.drawable.ic_baseline_bar_chart_24, R.drawable.ic_baseline_timer_24,R.drawable.ic_baseline_pin_drop_24};
+                ax = new int[]{ e.get(0).Cover,  e.get(1).Cover,
+                        e.get(2).Cover,  e.get(3).Cover};
                 this.selector_For_AlertDialog = 3;
                 break;
             default:
@@ -316,7 +330,38 @@ System.out.println("mano negah kon: " + dialogIs_On);
 
         builder.create().show();
     }
+    public ArrayList<Exercise>  populateExercises(){
+        ArrayList<Exercise> exercises=new ArrayList<Exercise>();
 
+        //chest
+        Exercise e1=new Exercise(1,R.drawable.benchpress,"Bench press","The bench press is a compound exercise that targets the muscles of the upper body. It involves lying on a bench and pressing weight upward using either a barbell or a pair of dumbbells. During a bench press, you lower the weight down to chest level and then press upwards while extending your arms");
+        Exercise e2=new Exercise(2,R.drawable.incline_benchpress,"Incline Bench press","The purpose of the incline press is to focus more of the work on the upper pecs. The main benefit in performing incline presses is to develop the upper portion of the pectoral muscles. When the bench is set at an incline (15 to 30 degrees), you activate your shoulders more since it's comparable to a shoulder press.");
+        Exercise e3=new Exercise(3,R.drawable.decline_benchpress,"Decline Bench press","In a decline bench press, the bench is set to 15 to 30 degrees on a decline. This angle places your upper body on a downward slope, which activates the lower pectoral muscles as you push weights away from your body. When part of a complete chest routine, decline bench presses can help your pecs look more defined.");
+        Exercise e4=new Exercise(4,R.drawable.chest_fly,"Chest Fly","Bring your arms up straight in front of you so they're at chest level, palms facing each other. Extend arms out to the sides, until your arms are extended. Keep arms at chest level the entire time. Bring them back to center. Repeat 10–15 times");
+        exercises.add(e1);
+        exercises.add(e2);
+        exercises.add(e3);
+        exercises.add(e4);
+        //arm
+        Exercise e5=new Exercise(5,R.drawable.bicep_curl,"Bicep Curl","an exercise in which you bend your arm towards your body at the elbow in order to make your biceps (= the large muscle at the front of the upper arm) stronger: Do three sets of biceps curls using 5- to 8-pound weights.");
+        Exercise e6=new Exercise(6,R.drawable.preacher_curl,"Preacher Curl"," a weightlifting exercise for the biceps in which a barbell is lifted by flexing the elbows, with the upper arms resting on an angled bench");
+        Exercise e7=new Exercise(7,R.drawable.triceps_extension,"Triceps Extension","Stand with feet hip width apart with a slight bend in the knees. Grip the bar with palms facing downwards, about shoulder width apart. Keeping your elbows close to your body, take a slight forward lean, and exhale as you push the bar down till your arms full extended.");
+        Exercise e8=new Exercise(8,R.drawable.dip_machine,"Dip Machine","The dip machine is a machine exercise that mimics a triceps dip, a bodyweight exercise performed on parallel bars or on a pull-up and dip station. It targets the triceps first, but also stretches and strengthens the chest and shoulders.");
+        exercises.add(e5);
+        exercises.add(e6);
+        exercises.add(e7);
+        exercises.add(e8);
+        //leg
+        Exercise e9=new Exercise(9,R.drawable.leg_extension,"Leg Extension","Leg extensions are done on a leg extension machine. You sit on the machine with a weighted pad on top of your lower legs. Then you use your quads to repeatedly extend your knees and lift your lower legs. While the leg extension is a great quad workout, it might not be the most practical move");
+        Exercise e10=new Exercise(10,R.drawable.leg_press,"Leg Press","Leg presses are done in a seated position. Your legs repeatedly press against weights, which can be adjusted according to your fitness level. This targets your quads, glutes, hamstrings, hips, and calves. The seated position of leg presses helps keep your upper body and torso still");
+        Exercise e11=new Exercise(11,R.drawable.dead_lift,"Dead lift","The deadlift is a movement in which your hips hinge backward to lower down and pick up a weighted barbell or kettlebell from the floor. Your back is flat throughout the movement. Some benefits of performing deadlifts include strengthening and gaining more definition in your upper and lower back, glutes, and hamstrings");
+        Exercise e12=new Exercise(12,R.drawable.leg_curl,"Leg Curl","The hamstring curl, also called a leg curl, is an exercise that strengthens the hamstrings. It involves bending your knees and moving your heels toward your butt while the rest of your body stays still. Typically, the exercise is done on a leg curl machine");
+        exercises.add(e9);
+        exercises.add(e10);
+        exercises.add(e11);
+        exercises.add(e12);
+        return  exercises;
+    }
 }
 
 
